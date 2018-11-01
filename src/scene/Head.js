4068c8319167c9659d4aff263572cc11623427b3
Marco Fugaro <marco.fugaro@gmail.com>
2018-11-01 16:22:18 +0100

    😺 Add cat head movement

--- src/scene/Head.js
+++ src/scene/Head.js
@@ -4,11 +4,28 @@ import { VERTICAL_GAP } from 'scene/Delimiters'
 import { shrimpsCollisionId } from 'scene/Shrimps'
 import { armsCollisionId } from 'scene/Arms'
 import { getRandomTransparentColor } from 'lib/three-utils'
+import assets from 'lib/AssetManager'
+import { mapRange, degToRad, lerp } from 'canvas-sketch-util/math'
+import eases from 'eases'
 
 // must be powers of 2!
 export const headCollisionId = 8
 
-export const HEAD_RADIUS = 2
+export const HEAD_RADIUS = 2.3
+
+// unit is degrees
+export const MAX_HEAD_ROTATION_X = 23
+export const MAX_HEAD_ROTATION_Y = 30
+
+const catHeadKey = assets.queue({
+  url: 'assets/cat-head.png',
+  type: 'texture',
+})
+
+const catHeadDisplacementKey = assets.queue({
+  url: 'assets/cat-head-displacement.png',
+  type: 'texture',
+})
 
 class Head extends CANNON.Body {
   mesh = new THREE.Object3D()
@@ -19,6 +36,30 @@ class Head extends CANNON.Body {
 
     const head = new CANNON.Sphere(HEAD_RADIUS)
     this.addShape(head)
+
+    const headMaterial = new THREE.MeshStandardMaterial({
+      displacementMap: assets.get(catHeadDisplacementKey),
+      displacementScale: 2,
+      // displacementBias: -0.428408,
+
+      metalness: 0,
+
+      map: assets.get(catHeadKey),
+
+      depthTest: false,
+      transparent: true,
+    })
+
+    const headGeometry = new THREE.PlaneGeometry(HEAD_RADIUS * 2, HEAD_RADIUS * 2, 32, 32)
+
+    const headMesh = new THREE.Mesh(headGeometry, headMaterial)
+
+    // position it
+    headMesh.scale.setScalar(1.5)
+    headMesh.position.y = 0.2
+    headMesh.position.z = 0.01
+
+    this.mesh.add(headMesh)
 
     if (window.DEBUG) {
       this.mesh.add(
@@ -41,6 +82,9 @@ export default class HeadComponent extends THREE.Object3D {
   material = new CANNON.Material('head')
   head
 
+  rotationX = 0
+  rotationY = 0
+
   constructor({ webgl, ...options }) {
     super(options)
     this.webgl = webgl
@@ -53,9 +97,6 @@ export default class HeadComponent extends THREE.Object3D {
       collisionFilterMask: shrimpsCollisionId | armsCollisionId,
       type: CANNON.Body.KINEMATIC,
       mass: 5,
-      // simulate the water
-      linearDamping: 0.99,
-      angularDamping: 0.99,
     })
 
     // position it
@@ -65,5 +106,33 @@ export default class HeadComponent extends THREE.Object3D {
     // and the mesh to the three.js scene
     this.webgl.world.addBody(this.head)
     this.add(this.head.mesh)
+  }
+
+  onTouchMove(e, [x, y]) {
+    this.rotationX = mapRange(x, 0, this.webgl.width, -MAX_HEAD_ROTATION_X, MAX_HEAD_ROTATION_X)
+    this.rotationY = mapRange(
+      y,
+      0,
+      this.webgl.height,
+      -MAX_HEAD_ROTATION_Y,
+      MAX_HEAD_ROTATION_Y * 0.2,
+    )
+  }
+
+  // follow the mouse but with a bit of lerping
+  update(dt = 0, time = 0) {
+    const targetRotationX = degToRad(this.rotationY)
+    const targetRotationY = degToRad(this.rotationX)
+
+    const currentRotation = new CANNON.Vec3()
+    this.head.quaternion.toEuler(currentRotation)
+
+    const currentRotationX = currentRotation.x
+    const currentRotationY = currentRotation.y
+
+    const rotationX = lerp(currentRotationX, targetRotationX, eases.quadInOut(dt * 10))
+    const rotationY = lerp(currentRotationY, targetRotationY, eases.quadInOut(dt * 10))
+
+    this.head.quaternion.setFromEuler(rotationX, rotationY, 0)
   }
 }
