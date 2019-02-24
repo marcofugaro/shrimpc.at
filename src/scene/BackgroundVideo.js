ad412c716f24771ba542b4c65555a8ab75b2c93a
Marco Fugaro <marco.fugaro@gmail.com>
2019-02-24 23:04:04 +0100

    🐛 fix issue with video on mobile

--- src/scene/BackgroundVideo.js
+++ src/scene/BackgroundVideo.js
@@ -1,30 +1,52 @@
 import * as PIXI from 'pixi.js'
+import pEvent from 'p-event'
 
 export default class BackgroundVideo extends PIXI.Container {
   constructor({ webgl, ...options }) {
     super(options)
+    this.webgl = webgl
 
+    this.load()
+  }
+
+  // it needs to be async because of the video loading stuff,
+  // otherwise this would stay in the constructor
+  async load() {
     // Render the video in pixi as a pixi layer
-    const video = document.querySelector('video')
-    const videoTexture = PIXI.Texture.from(video)
+    const url = 'assets/3d-fish-school.mp4'
+    const videoTexture = PIXI.Texture.from(url, {
+      // cap the video at 30, the source video framerate,
+      // otherwise it would be the renderer's fps
+      // BUG if I uncomment this line the shrimps bug out 🤔
+      // resourceOptions: { updateFPS: 30 },
+    })
+
+    const video = videoTexture.baseTexture.resource.source
+
+    video.muted = true
+    video.loop = true
+
+    // wait for the video to load so we have its width and height
+    await pEvent(video, 'canplay')
+
     const videoSprite = new PIXI.Sprite(videoTexture)
 
-    const rendererRatio = webgl.pixi.renderer.width / webgl.pixi.renderer.height
+    const rendererRatio = this.webgl.pixi.renderer.width / this.webgl.pixi.renderer.height
     const ratio = video.videoWidth / video.videoHeight
 
     // like backgorund-size: cover
     if (ratio > rendererRatio) {
-      videoSprite.height = webgl.pixi.renderer.height
+      videoSprite.height = this.webgl.pixi.renderer.height
       videoSprite.width = videoSprite.height * ratio
     } else {
-      videoSprite.width = webgl.pixi.renderer.width
+      videoSprite.width = this.webgl.pixi.renderer.width
       videoSprite.height = videoSprite.width * (1 / ratio)
     }
 
     // center it
     videoSprite.anchor.set(0.5)
-    videoSprite.x = webgl.pixi.renderer.width / 2
-    videoSprite.y = webgl.pixi.renderer.height / 2
+    videoSprite.x = this.webgl.pixi.renderer.width / 2
+    videoSprite.y = this.webgl.pixi.renderer.height / 2
 
     this.addChild(videoSprite)
 
